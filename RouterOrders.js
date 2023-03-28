52d0e92ce34ce2c67e0bfbd088a0d06b9ffabf2e
Uyansu <suat1593@web.de>
2023-03-28 14:35:41 +0200

    push after GET/:id command for orders

--- RouterOrders.js
+++ RouterOrders.js
@@ -13,4 +13,14 @@ routerOrders.get("/orders", async (req, res) => {
     }
 });
 
+routerOrders.get("/orders/:id", async (req, res) => {
+    const {id}  = req.params;
+    try {
+        const {rows} = await pool.query("SELECT * FROM orders WHERE id=$1", [id]);
+        res.json({data: rows});
+    } catch (error) {
+        res.sendStatus(403);
+    }
+})
+
 module.exports = routerOrders;
